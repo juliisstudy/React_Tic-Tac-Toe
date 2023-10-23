54957213bd2dbc1c1674ca3dc06dbade644a7973
Juli <julianalicode@gmail.com>
2023-10-23 15:37:41 -0400

    Create a game board

---
+++ src/Square.js
@@ -0,0 +1,10 @@
+export default function Square({value}){
+
+    function handleClick(){
+        console.log("click");
+    }
+    return(
+        <button className="square" onClick={handleClick}>{value}</button>
+    )
+}
+
